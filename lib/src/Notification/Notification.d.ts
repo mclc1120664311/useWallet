49be8c841d6ab512df3dc1bd32655ffa770ea8d7
mclc1120664311 <1120664311@qq.com>
2022-06-23 08:46:41 +0800

    feat: 修改npm包出口文件

--- lib/src/Notification/Notification.d.ts
+++
@@ -1,4 +0,0 @@
-import "antd/es/notification/style/css";
-import "./Notification.less";
-declare const openNotification: (type?: number[], description?: string) => void;
-export default openNotification;
--- lib/src/utils.d.ts
+++
@@ -1,4 +0,0 @@
-export declare function fotmatErrorMsg(err: string): string;
-export declare function toHome(): void;
-export declare const decimalToHex: (decimal: any) => string;
-export declare const errorFunction: (err: any) => void;
