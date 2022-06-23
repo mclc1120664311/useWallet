a2e99358b7814fb59f7b8bd4b169d70b29450028
mclc1120664311 <1120664311@qq.com>
2022-06-23 12:32:31 +0800

    feat: rollup build

--- rollup.config.js
+++ rollup.config.js
@@ -1,20 +1,34 @@
-import less from 'rollup-plugin-less';
+import postcss from 'rollup-plugin-postcss'
+
+
 import { uglify } from 'rollup-plugin-uglify'
 import typescript from 'rollup-plugin-typescript2'
 
-import pkg from './package.json'
+// import pkg from './package.json'
 
 export default {
   input: 'src/index.ts',
   output: [
     {
-      file: pkg.main,
+      file: './dist/my-lib-umd.js',
+      format: 'umd',
+      name: 'myLib'
+      //当入口文件有export时，'umd'格式必须指定name
+      //这样，在通过<script>标签引入时，才能通过name访问到export的内容。
+    },
+    {
+      file: './dist/index.es.js',
+      format: 'es'
+    },
+    {
+      file: './dist/index.cjs.js',
       format: 'cjs',
       exports: 'named',
       sourcemap: true,
       strict: false,
     },
+
   ],
-  plugins: [less(), typescript(), uglify()],
+  plugins: [postcss(), typescript()],
   external: ['react', 'react-dom'],
 }
