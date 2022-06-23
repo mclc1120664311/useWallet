09b108f7adeefba02d121e37bcfd28ae96f38a17
mclc1120664311 <1120664311@qq.com>
2022-06-23 17:48:07 +0800

    feat: add test

--- src/hooks/useWallet/__tests__/index.test.ts
+++ src/hooks/useWallet/__tests__/index.test.ts
@@ -1,4 +1,4 @@
-import { renderHook, act } from '@testing-library/react-hooks';
+import { renderHook } from '@testing-library/react-hooks';
 import { useWallet } from '../index';
 
 describe('useWallet tests', () => {
@@ -7,9 +7,10 @@ describe('useWallet tests', () => {
   });
 
   it('renders the hook correctly and checks types', () => {
-    const { result } = renderHook(() => useWallet(['0x61']));
-    expect(result.current.count).toBe(0);
-    expect(typeof result.current.count).toBe('number');
-    expect(typeof result.current.increment).toBe('function');
+    const { result } = renderHook(() => useWallet({ supportedChainIds: [97] }));
+    expect(result.current.address).toBe('string');
+    expect(result.current.address).toBe(0);
+    expect(typeof result.current.address).toBe('number');
+    expect(typeof result.current.address).toBe('function');
   });
 });
